fc9b58faabf505d4f6208b854f0a8ed75805243b
Dr-AnasAlazab <anas.alazab30@gmail.com>
2025-05-11 23:05:59 +0300

    a

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -111,7 +111,7 @@ export const BentoGridItem = ({
             {title}
           </div>
 
-          {/* {id === 2 && <GlobeDemo />} */}
+          {id === 2 && <GlobeDemo />}
 
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
@@ -148,7 +148,7 @@ export const BentoGridItem = ({
                   copied ? "block" : "block"
                 }`}
               >
-                <Lottie
+                {/* <Lottie
                   options={{
                     loop: copied,
                     autoplay: copied,
@@ -159,7 +159,7 @@ export const BentoGridItem = ({
                   }}
                   height={200}
                   width={400}
-                />
+                /> */}
               </div>
 
               <MagicButton
